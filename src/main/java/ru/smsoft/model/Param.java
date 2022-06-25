57b43a2586e540c75743aeff9ac6a48269ccc563
anton-lapin <lapin_antoine@mail.ru>
2022-06-25 16:31:47 +0300

    add lombock, refactor

--- src/main/java/ru/smsoft/model/Param.java
+++ src/main/java/ru/smsoft/model/Param.java
@@ -1,52 +1,13 @@
 package ru.smsoft.model;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@AllArgsConstructor
+@Data
 public class Param {
 
     private String name;
 
     private String value;
-
-    public Param(String name, String value) {
-        this.name = name;
-        this.value = value;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Param param = (Param) o;
-        return Objects.equals(name, param.name) && Objects.equals(value, param.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, value);
-    }
-
-    @Override
-    public String toString() {
-        return "Param{" +
-                "name='" + name + '\'' +
-                ", value='" + value + '\'' +
-                '}';
-    }
 }
--- src/main/java/ru/smsoft/replacer/Replacer.java
+++ src/main/java/ru/smsoft/replacer/Replacer.java
@@ -6,22 +6,20 @@ import ru.smsoft.parser.FileParser;
 
 import java.nio.file.Path;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Replacer {
 
     public static void replace(Path cf, Path replacement) {
         List<Param> fromProject = FileParser.parse(cf);
         List<Param> fromReplacement = FileParser.parse(replacement);
-
-        for (Param rep: fromReplacement) {
-            for (Param prj: fromProject) {
-                if (rep.getName().equals(prj.getName())) {
-                    prj.setValue(rep.getValue());
-                }
-            }
-        }
-
-        FileInserter.insert(cf, fromProject);
+        List<String> repNames = fromReplacement.stream().map(Param::getName)
+                .collect(Collectors.toList());
+        List<Param> filtered = fromProject.stream()
+                .filter(p -> !repNames.contains(p.getName()))
+                .collect(Collectors.toList());
+        filtered.addAll(fromReplacement);
+        FileInserter.insert(cf, filtered);
     }
 
 
